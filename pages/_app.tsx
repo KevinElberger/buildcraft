78d2cd82836e813698060d672b63948ba0972fe5
Kevin Elberger <kevelberger@gmail.com>
2021-12-14 09:13:35 -0800

    Add styled-components, add global styles and temp theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from '../styles/global';
+import { lightTheme } from '../styles/theme.config';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={lightTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp
---
+++ styles/theme.config.ts
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  body: '#fffff',
+  main: '#5C14DB',
+  mainColor: '#FFFFFF',
+  accent: '#E5DE17',
+  accentColor: '#161616',
+  secondary: '#FFFFFF',
+  secondaryColor: '#343434',
+  dullColor: '#343434',
+  ternary: '#000000',
+  codeColor: '#D121C5',
+}
+
