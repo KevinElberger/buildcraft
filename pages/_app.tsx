3ce18cca98f9f5476cdac3c0acecc40f0c0ec889
Kevin Elberger <kevelberger@gmail.com>
2021-12-14 13:31:13 -0800

    Add react-redux and redux toolkit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
 import { ThemeProvider } from "styled-components";
+import { store } from '../store/store';
 import { GlobalStyles } from '../styles/global';
 import { lightTheme } from '../styles/theme.config';
 
@@ -8,7 +10,9 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <GlobalStyles />
       <ThemeProvider theme={lightTheme}>
-        <Component {...pageProps} />
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
       </ThemeProvider>
     </>
   );
