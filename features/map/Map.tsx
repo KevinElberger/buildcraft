541ddf01559f3e11a4db637b0954ea700dc8dbe7
Kevin Elberger <kevelberger@gmail.com>
2021-12-29 09:37:04 -0800

    Remove Toolbar component

--- features/map/Map.tsx
+++ features/map/Map.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import { Grid } from './components/Grid';
-import { Toolbar } from './components/Toolbar';
 
 export default function Map() {
 
